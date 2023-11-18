4e4efa56d92e9275e081a44c4fc95aa118fe6361
Vermouth2004 <frodobaggines1954@gmail.com>
2023-11-18 19:52:11 +0900

    投稿ページヘッダーの実装

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
 class SongsController < ApplicationController
   def index
   end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'songs/index'
   root to: "songs#index"
+  resources :songs, only: [:index, :new]
+
 end
